ab25053c407c2cb8606437384dae58040790b07d
DennisWarchocki <dw.warchocki@gmail.com>
2022-08-07 01:30:32 +0200

    beta 1.0 hotfix

--- components/string.js
+++ components/string.js
@@ -1,5 +1,41 @@
-function myFunction() {
-    var x = document.getElementById("hide");
+function aFunction() {
+    var x = document.getElementById("answer__1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function bFunction() {
+    var x = document.getElementById("answer__2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function cFunction() {
+    var x = document.getElementById("answer__3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function dFunction() {
+    var x = document.getElementById("answer__4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function eFunction() {
+    var x = document.getElementById("answer__5");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
